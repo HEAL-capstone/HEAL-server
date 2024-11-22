d6b3c81dab7fb3ff9ef6e78d5c75d342c316e245
goorm <goorm@goorms-MacBook-Pro.local>
2024-11-23 02:29:13 +0900

    feat: 중간 테이블 생성 + interest 테이블 기본값 설정

--- database_init/init.sql
+++ database_init/init.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS heal_db;
 
 -- user 테이블 생성
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
     user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     gender ENUM('male', 'female') NOT NULL,
@@ -12,16 +12,46 @@ CREATE TABLE user (
 );
 
 -- interests 테이블 생성
-CREATE TABLE interests (
+CREATE TABLE IF NOT EXISTS interests (
     interests_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     category VARCHAR(255) NOT NULL
 );
 
+INSERT INTO interests (category) VALUES
+('간 건강'),
+('피로 개선'),
+('눈 건강'),
+('관절/뼈 건강'),
+('면역력 강화'),
+('소화 건강'),
+('수면 개선'),
+('스트레스 관리'),
+('피부 건강'),
+('혈액순환');
+
 -- user_interests 중간 테이블 생성
-CREATE TABLE user_interests (
+CREATE TABLE IF NOT EXISTS user_interests (
     user_interest_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
     interests_id BIGINT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user(user_id),
     FOREIGN KEY (interests_id) REFERENCES interests(interests_id)
 );
+
+-- supplements 테이블 생성
+CREATE TABLE IF NOT EXISTS supplements (
+    supplements_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    supplement_name VARCHAR(50) NOT NULL,
+    supplement_description TEXT
+);
+
+-- supplement_interests 중간 테이블 생성
+CREATE TABLE IF NOT EXISTS supplement_interests (
+    supplement_interest_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    supplements_id BIGINT NOT NULL,
+    interests_id BIGINT NOT NULL,
+    UNIQUE (supplements_id, interests_id),  -- 중복 방지
+    FOREIGN KEY (supplements_id) REFERENCES supplements(supplements_id) ON DELETE CASCADE,
+    FOREIGN KEY (interests_id) REFERENCES interests(interests_id) ON DELETE CASCADE
+);
+
